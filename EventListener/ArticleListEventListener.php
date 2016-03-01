342320ee976e4284b479e9e8e4a8642d6f6a862d
İsmail BASKIN <ismailbaskin1@gmail.com>
2016-03-01 17:02:18 +0200

    show doi button if possible

--- EventListener/ArticleListEventListener.php
+++ EventListener/ArticleListEventListener.php
@@ -4,6 +4,7 @@ namespace OkulBilisim\OjsDoiBundle\EventListener;
 
 use APY\DataGridBundle\Grid\Action\RowAction;
 use APY\DataGridBundle\Grid\Column\ActionsColumn;
+use APY\DataGridBundle\Grid\Row;
 use Ojs\JournalBundle\Event\Article\ArticleEvents;
 use Ojs\JournalBundle\Event\ListEvent;
 use Ojs\JournalBundle\Service\JournalService;
@@ -38,25 +39,41 @@ class ArticleListEventListener implements EventSubscriberInterface
      */
     public function onListInitialized(ListEvent $event)
     {
+
+
         $journal = $this->journalService->getSelectedJournal();
         $grid = $event->getGrid();
-
         /** @var ActionsColumn $actionColumn */
         $actionColumn = $grid->getColumn("actions");
         $rowActions = $actionColumn->getRowActions();
 
         $rowAction = new RowAction('<i class="fa fa-copyright"></i>', 'okul_bilisim_doi_doi_article_doi');
-        $rowAction->setAttributes(
-            [
-                'class' => 'btn btn-primary btn-xs',
-                'data-toggle' => 'tooltip',
-                'title' => 'Get DOI',
-            ]
+
+        $rowAction->manipulateRender(
+            function (RowAction $rowAction, Row $row) use ($journal) {
+                if (1 === $row->getField('status') && $row->getField('pubdate') >= new \DateTime(
+                        '2014-01-01'
+                    ) && '' !== $row->getField('issue.translations.title')
+                ) {
+                    $rowAction->setAttributes(
+                        [
+                            'class' => 'btn btn-primary btn-xs',
+                            'data-toggle' => 'tooltip',
+                            'title' => 'Get DOI',
+                        ]
+                    );
+                    $rowAction->setRouteParameters(['id', 'journalId' => $journal->getId()]);
+
+                    return $rowAction;
+                }
+                return null;
+            }
         );
-        $rowAction->setRouteParameters(['id', 'journalId' => $journal->getId()]);
 
         $rowActions[] = $rowAction;
         $actionColumn->setRowActions($rowActions);
+
+
         $event->setGrid($grid);
     }
 }
