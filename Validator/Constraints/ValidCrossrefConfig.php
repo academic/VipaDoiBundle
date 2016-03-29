ea5e6fef3c5faac2d158b6718bcf1d2d3a1956de
İsmail BASKIN <ismailbaskin1@gmail.com>
2016-03-29 17:15:15 +0300

    add custom crossref constraint

---
+++ Validator/Constraints/ValidCrossrefConfig.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace BulutYazilim\OjsDoiBundle\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @Annotation
+ */
+class ValidCrossrefConfig extends Constraint
+{
+    public $username = 'username';
+
+    public $password = 'password';
+
+    public $prefix = 'prefix';
+
+    public $usernameMessage = 'Kullanıcı adı bulunamadı.';
+
+    public $passwordMessage = 'Parola bulunamadı.';
+
+    public $prefixMessage = 'DOI ön ekiniz hatalı. Şunlardan biri olmalı : %s';
+
+    public function getTargets()
+    {
+        return self::CLASS_CONSTRAINT;
+    }
+}
---
+++ Validator/Constraints/ValidCrossrefConfigValidator.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace BulutYazilim\OjsDoiBundle\Validator\Constraints;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class ValidCrossrefConfigValidator extends ConstraintValidator
+{
+    /**
+     * @param \BulutYazilim\OjsDoiBundle\Entity\CrossrefConfig $crossrefConfig
+     * @param Constraint $constraint
+     */
+    public function validate($crossrefConfig, Constraint $constraint)
+    {
+        /** @var ValidCrossrefConfig $constraint */
+        $client = new Client();
+
+        try {
+            $result = $client->request(
+                'GET',
+                'http://doi.crossref.org/info?usr='.$crossrefConfig->getUsername().'&pwd='.$crossrefConfig->getPassword(
+                ).'&rtype=prefixes'
+            );
+            $jsonResult = json_decode($result->getBody()->getContents(), true);
+            $allowedPrefixes = $jsonResult['allowed-prefixes'];
+            if (!in_array($crossrefConfig->getPrefix(), $allowedPrefixes, true)) {
+                $this->context->buildViolation(sprintf($constraint->prefixMessage, implode(',', $allowedPrefixes)))
+                    ->atPath($constraint->prefix)
+                    ->addViolation();
+            }
+        } catch (RequestException $e) {
+            if (strpos($e->getMessage(), 'does not seem to exist') !== false) {
+                $this->context->buildViolation($constraint->usernameMessage)
+                    ->atPath($constraint->username)
+                    ->addViolation();
+            } else {
+                $this->context->buildViolation($constraint->passwordMessage)
+                    ->atPath($constraint->password)
+                    ->addViolation();
+            }
+        }
+
+    }
+}
