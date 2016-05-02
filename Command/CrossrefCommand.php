007de121af3ee45b19cdba38a596069dccfb9019
Behram ÇELEN <behramcelen@gmail.com>
2016-05-02 19:30:36 +0300

    revise article statuses, refactor code

--- Command/CrossrefCommand.php
+++ Command/CrossrefCommand.php
@@ -69,7 +69,6 @@ class CrossrefCommand extends ContainerAwareCommand
                     continue;
                 }
 
-
                 $responseObject = json_decode($response->getBody()->getContents(), true);
 
                 $message = $responseObject['message'];
