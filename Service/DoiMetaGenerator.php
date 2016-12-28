bf4d02e4260a7a59a181c98d74b0477d267e5a94
Hüseyin Mert <huseyinin@gmail.com>
2016-12-28 12:20:18 +0300

    CrossRef only accepts lower cases.

--- Service/DoiMetaGenerator.php
+++ Service/DoiMetaGenerator.php
@@ -79,7 +79,7 @@ class DoiMetaGenerator
         }
 
         $doi->body->journal->journalArticle->publicationDate->setDate($accessor->getValue($article, 'pubdate'));
-        $doi->body->journal->journalArticle->language = $article->getPrimaryLanguage();
+        $doi->body->journal->journalArticle->language = strtolower($article->getPrimaryLanguage());
 
         if ($article->getFirstPage() == null && $article->getLastPage() == null) {
             $doi->body->journal->journalArticle->pages = null;
